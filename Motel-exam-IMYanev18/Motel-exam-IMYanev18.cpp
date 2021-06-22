7746bab3062f214cf36a40124b32a70c788ba545
IMYanev18 <IMYanev18@codingburgas.bg>
2021-06-22 09:12:44 +0300

    initial commit

--- Motel-exam-IMYanev18/Motel-exam-IMYanev18.cpp
+++ Motel-exam-IMYanev18/Motel-exam-IMYanev18.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
+
+struct ROOM_PRICING
+{
+    float singleRoomPrice = 50;
+    float doubleRoomPrice = 90;
+    float maisonettePrice = 135;
+
+};
+
+class Motel
+{
+public:
+
+    ROOM_PRICING getRoomPricing()
+    {
+        ROOM_PRICING roomPrice;
+
+        return roomPrice;
+    }
+
+
+    string rentRoom(string clientName,string roomType,int numberOfNights)
+    {
+        
+    }
+
+
+    Motel(string name, int capacity)
+    {
+        double singleRoomPrice;
+        double doubleRoomPrice;
+        double maisonettePrice;
+    }
+
+
+
+private:
+    string name;
+    int capacity;
+    vector<int> bookings;
+    int currentBookingNumber = 1;
+    int singleRooms;
+    int doubleRooms;
+    int maisonette;
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
